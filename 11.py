75b2df25142d7175b7e53e9528ed50fe4a35409b
test <test@mail.ru>
2024-12-01 13:49:01 +0300

    comit 11

---
+++ 11.py
@@ -0,0 +1,2 @@
+from game import b
+b()
---
+++ victory1.py
@@ -0,0 +1,15 @@
+from victory import a
+
+print('привет')
+
+name = input('как тебя зовут?:')
+
+b = input('готов ли ты играть?:')
+
+if b == "да":
+    a()
+elif b == "нет":
+    print(("ну давай готовисьтя тогда"))
+
+else:
+    print()
